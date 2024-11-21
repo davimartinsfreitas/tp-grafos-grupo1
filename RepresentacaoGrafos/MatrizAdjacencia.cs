ab121ddfc2c10f7ea91de2ba8863a6f26933ca34
Jean Machado <jean.c.m.amaral@gmail.com>
2024-11-21 19:54:15 -0300

    Created Adjacency Matrix

--- RepresentacaoGrafos/MatrizAdjacencia.cs
+++ RepresentacaoGrafos/MatrizAdjacencia.cs
@@ -8,5 +8,32 @@ namespace tp_grafos.RepresentacaoGrafos
 {
     internal class MatrizAdjacencia
     {
+        private int[,] matriz;
+
+        public MatrizAdjacencia(int vertices)
+        {
+            matriz = new int[vertices, vertices];
+        }
+
+        public void AdicionarAresta(int origem, int destino, int peso)
+        {
+            matriz[origem, destino] = peso;
+            matriz[destino, origem] = peso; // Caso seja um grafo não direcionado
+        }
+
+        public void Imprimir()
+        {
+            Console.WriteLine("Matriz de Adjacência:");
+            int tamanho = matriz.GetLength(0);
+            for (int i = 0; i < tamanho; i++)
+            {
+                for (int j = 0; j < tamanho; j++)
+                {
+                    Console.Write($"{matriz[i, j]} ");
+                }
+                Console.WriteLine();
+            }
+        }
     }
 }
+
