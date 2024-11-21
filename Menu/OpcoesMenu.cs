4fcf94e7e79d510d49700a157b9368b71e9cbf71
Davi Fonseca <davi.fonseca.1405489@sga.pucminas.br>
2024-11-21 12:59:35 -0300

    creates initial project structure

---
+++ Menu/OpcoesMenu.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tp_grafos.Menu
+{
+    public enum OpcoesMenu
+    {
+      CRIAR_GRAFO,
+      IMPRIMIR_GRAFO,
+      LER_GRAFO_DIMACS,
+      IMPRIMIR_ARESTAS_ADJACENTES,
+      IMPRIMIR_VERTICES_ADJACENTES,
+      IMPRIMIR_ARESTAS_INCIDENTES_A_VERTICE,
+      IMPRIMIR_VERTICES_INCIDENTES_A_ARESTA,
+      VERIFICAR_VERTICES_ADJACENTES,
+      SUBSTITUIR_PESO_ARESTA,
+      TROCAR_VERTICES,
+      BUSCA_EM_LARGURA,
+      BUSCA_EM_PROFUNDIDADE,
+      CAMINHO_MINIMO_DIJKSTRA,
+      CAMINHO_MINIMO_FLOYD_WARSHALL,
+      DESAFIO_FERROVIAS,
+      SAIR
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,10 +1,95 @@
-﻿namespace tp_grafos
+﻿using System.Text;
+using tp_grafos.Menu;
+
+namespace tp_grafos
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            
+            OpcoesMenu opcaoUsuario;
+
+            do
+            {
+                Console.WriteLine(exibeMenu());
+                opcaoUsuario = (OpcoesMenu) int.Parse(Console.ReadLine());
+                processarMenu(opcaoUsuario);
+            } while (opcaoUsuario != OpcoesMenu.SAIR);
+        }
+
+
+        public static String exibeMenu()
+        {
+            StringBuilder menu = new StringBuilder();
+            menu.AppendLine("\n=====Parte 1======");
+            int index = 1;
+            menu.AppendLine($"{index++}. Criar grafo e imprimi-lo");
+            menu.AppendLine("\n=====Parte 2======");
+            menu.AppendLine($"{index++}. Ler grafo no formato DIMACS");
+            menu.AppendLine($"{index++}. Imprimir Arestas Adjacentes a uma aresta");
+            menu.AppendLine($"{index++}. Imprimir Vértices Adjacentes a um vértice");
+            menu.AppendLine($"{index++}. Imprimir Arestas Incidentes a um vértice");
+            menu.AppendLine($"{index++}. Imprimir Vértices Incidentes a uma aresta");
+            menu.AppendLine($"{index++}. Verificar se dois vértices são adjacentes");
+            menu.AppendLine($"{index++}. Substitituir peso de uma aresta");
+            menu.AppendLine($"{index++}. Trocar dois vértices");
+            menu.AppendLine($"{index++}. Executar busca em largura");
+            menu.AppendLine($"{index++}. Executar busca em profundidade");
+            menu.AppendLine($"{index++}. Executar algoritmo de Dikstra");
+            menu.AppendLine($"{index++}. Executar algoritmo de Floyd Warshall");
+
+            menu.AppendLine("\n=====Parte 3======");
+            menu.AppendLine($"{index++}. Desafio - Ferrovias");
+
+            menu.AppendLine($"\n{index++}. Sair");
+
+            menu.AppendLine("\nEscolha uma opção");
+
+            return menu.ToString();
+        }
+
+        public static void processarMenu(OpcoesMenu opcaoUsuario)
+        {
+            switch (opcaoUsuario)
+            {
+                case OpcoesMenu.CRIAR_GRAFO:
+                    break;
+                case OpcoesMenu.LER_GRAFO_DIMACS:
+                    break;
+                case OpcoesMenu.IMPRIMIR_ARESTAS_ADJACENTES:
+                    break;
+                case OpcoesMenu.IMPRIMIR_VERTICES_ADJACENTES:
+                    break;
+                case OpcoesMenu.IMPRIMIR_ARESTAS_INCIDENTES_A_VERTICE:
+                    break;
+                case OpcoesMenu.IMPRIMIR_VERTICES_INCIDENTES_A_ARESTA:
+                    break;
+                case OpcoesMenu.VERIFICAR_VERTICES_ADJACENTES:
+                    break;
+                case OpcoesMenu.SUBSTITUIR_PESO_ARESTA:
+                    break;
+                case OpcoesMenu.TROCAR_VERTICES:
+                    break;
+                case OpcoesMenu.BUSCA_EM_LARGURA:
+                    break;
+                case OpcoesMenu.BUSCA_EM_PROFUNDIDADE:
+                    break;
+                case OpcoesMenu.CAMINHO_MINIMO_DIJKSTRA:
+                    break;
+                case OpcoesMenu.CAMINHO_MINIMO_FLOYD_WARSHALL:
+                    break;
+                case OpcoesMenu.DESAFIO_FERROVIAS:
+                    break;
+                case OpcoesMenu.SAIR:
+                    Console.WriteLine("Saindo...");
+                    break;
+                default:
+                    Console.WriteLine("Opção inválida");
+                    break;
+            }
+          
         }
     }
+
+
 }
